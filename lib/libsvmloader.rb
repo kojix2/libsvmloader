1177e7a5429551cdfb85e367646f52331554c93f
yoshoku <yoshoku@outlook.com>
2018-08-25 10:53:43 +0900

    :rocket: Fix type of sample and label to be Ruby Array

--- lib/libsvmloader.rb
+++ lib/libsvmloader.rb
@@ -1,92 +1,112 @@
+# frozen_string_literal: true
 
 require 'libsvmloader/version'
-require 'nmatrix/nmatrix'
+require 'csv'
 
 # LibSVMLoader loads (and dumps) dataset file with the libsvm file format.
 class LibSVMLoader
   class << self
-    # Load a dataset with the libsvm file format into NMatrix.
+    # Load a dataset with the libsvm file format.
     #
-    # @param filename    [String]  A path to a dataset file.
+    # @param filename    [String]  Path to a dataset file.
     # @param zero_based  [Boolean] Whether the column index starts from 0 (true) or 1 (false).
-    # @param stype       [Symbol]  The strorage type of the nmatrix consisting of feature vectors.
-    # @param label_dtype [Symbol]  The data type of the NMatrix consisting of labels or target values.
-    # @param value_dtype [Symbol]  The data type of the NMatrix consisting of feature vectors.
+    # @param label_dtype [String]  Data type of labels or target values ('int', 'float', 'complex').
+    # @param value_dtype [String]  Data type of feature vectors ('int', 'float', 'complex').
     #
-    # @return [Array<NMatrix>]
+    # @return [Array<Array>]
     #   Returns array containing the (n_samples x n_features) matrix for feature vectors
-    #   and (n_samples x 1) matrix for labels or target values.
-    def load_libsvm_file(filename, zero_based: false, stype: :yale, label_dtype: :int32, value_dtype: :float64)
-      ftvecs = []
+    #   and (n_samples) vector for labels or target values.
+    def load_libsvm_file(filename, zero_based: false, label_dtype: 'int', value_dtype: 'float')
       labels = []
-      n_features = 0
-      File.read(filename).split("\n").each do |line|
-        label, ftvec, max_idx = parse_libsvm_line(line, zero_based)
+      ftvecs = []
+      maxids = []
+      label_class = parse_dtype(label_dtype)
+      value_class = parse_dtype(value_dtype)
+      CSV.foreach(filename, col_sep: "\s", headers: false) do |row|
+        label, ftvec, maxid = parse_libsvm_row(row, zero_based, label_class, value_class)
         labels.push(label)
         ftvecs.push(ftvec)
-        n_features = [n_features, max_idx].max
+        maxids.push(maxid)
       end
-      [convert_to_nmatrix(ftvecs, n_features, value_dtype, stype),
-       NMatrix.new([labels.size, 1], labels, dtype: label_dtype)]
+      [convert_to_matrix(ftvecs, maxids.max + 1, value_class), labels]
     end
 
     # Dump the dataset with the libsvm file format.
     #
-    # @param data       [NMatrix] (n_samples x n_features) matrix consisting of feature vectors.
-    # @param labels     [NMatrix] (n_samples x 1) matrix consisting of labels or target values.
-    # @param filename   [String]  A path to the output libsvm file.
+    # @param data       [Array] (n_samples x n_features) matrix consisting of feature vectors.
+    # @param labels     [Array] (n_samples) vector consisting of labels or target values.
+    # @param filename   [String]  Path to the output libsvm file.
     # @param zero_based [Boolean] Whether the column index starts from 0 (true) or 1 (false).
     def dump_libsvm_file(data, labels, filename, zero_based: false)
-      n_samples = [data.rows, labels.rows].min
-      label_type = detect_dtype(labels)
-      value_type = detect_dtype(data)
+      n_samples = [data.size, labels.size].min
+      label_format = detect_format(labels.first)
+      value_format = detect_format(data.flatten.first)
       File.open(filename, 'w') do |file|
-        n_samples.times do |n|
-          file.puts(dump_libsvm_line(labels[n], data.row(n),
-                                     label_type, value_type, zero_based))
-        end
+        n_samples.times { |n| file.puts(dump_libsvm_line(labels[n], data[n], label_format, value_format, zero_based)) }
       end
     end
 
     private
 
-    def parse_libsvm_line(line, zero_based)
-      tokens = line.split
-      label = tokens.shift.to_f
-      ftvec = tokens.map do |el|
+    def parse_libsvm_row(row, zero_based, label_type, value_type)
+      label = convert_type(row.shift, label_type)
+      ftvec = row.map do |el|
         idx, val = el.split(':')
-        idx = idx.to_i - (zero_based == false ? 1 : 0)
-        [idx, val.to_f]
+        [idx.to_i - (zero_based == false ? 1 : 0), convert_type(val, value_type)]
       end
-      max_idx = ftvec.map { |el| el[0] }.max
-      max_idx ||= 0
+      max_idx = ftvec.map { |idx, _val| idx }.max || 0
       [label, ftvec, max_idx]
     end
 
-    def convert_to_nmatrix(data, n_features, value_dtype, stype)
-      n_samples = data.size
-      mat = NMatrix.zeros([n_samples, n_features + 1],
-                          dtype: value_dtype, stype: stype)
-      data.each_with_index do |ftvec, n|
-        ftvec.each do |el|
-          mat[n, el[0]] = el[1]
-        end
+    def parse_dtype(dtype)
+      case dtype.to_s
+      when /^(int)/i
+        :int
+      when /^(float)/i
+        :float
+      when /^(complex)/i
+        :complex
+      else
+        :string
       end
-      mat
     end
 
-    def detect_dtype(data)
+    def convert_type(value, dtype)
+      case dtype
+      when :int
+        value.to_i
+      when :float
+        value.to_f
+      when :complex
+        value.to_c
+      else
+        value
+      end
+    end
+
+    def convert_to_matrix(data, n_features, value_type)
+      z = convert_type(0, value_type)
+      data.map do |ft|
+        vec = Array.new(n_features) { z }
+        ft.each { |idx, val| vec[idx] = val }
+        vec
+      end
+    end
+
+    def detect_format(data)
       type = '%s'
-      type = '%d' if %i[int8 int16 int32 int64].include?(data.dtype)
-      type = '%.10g' if %i[float32 float64].include?(data.dtype)
+      type = '%d' if data.is_a?(Integer)
+      type = '%.10g' if data.is_a?(Float)
       type
     end
 
-    def dump_libsvm_line(label, ftvec, label_type, value_type, zero_based)
-      line = format(label_type.to_s, label)
-      ftvec.to_a.each_with_index do |val, n|
-        idx = n + (zero_based == false ? 1 : 0)
-        line += format(" %d:#{value_type}", idx, val) if val != 0.0
+    def dump_libsvm_line(label, ftvec, label_format, value_format, zero_based)
+      line = format(label_format, label)
+      ftvec.each_with_index do |val, n|
+        unless val.zero?
+          idx = n + (zero_based == false ? 1 : 0)
+          line += format(" %d:#{value_format}", idx, val)
+        end
       end
       line
     end
