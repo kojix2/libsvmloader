540a9825f2d5dbdcdb6ca0edb81329f805f43fdb
yoshoku <yoshoku@outlook.com>
2021-06-12 13:52:51 +0900

    :wrench: Fix to use Github actions for checking code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-require 'coveralls'
+require 'simplecov-lcov'
 
-Coveralls.wear!
+SimpleCov::Formatter::LcovFormatter.config do |config|
+  config.report_with_single_file = true
+  config.single_report_path = 'coverage/lcov.info'
+end
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter,
-                                                                Coveralls::SimpleCov::Formatter])
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [SimpleCov::Formatter::HTMLFormatter, SimpleCov::Formatter::LcovFormatter]
+)
+
 SimpleCov.start
 
 require 'bundler/setup'
